ba82e70f655ca0a7242fd3d533d3d433046b8455
Maxime Colliat <mcolliat21@gmail.com>
2025-07-04 09:14:49 +0200

    add api_bench.py

--- src/structs/app_state.rs
+++ src/structs/app_state.rs
@@ -1,6 +1,14 @@
-use sqlx::{PgPool};
+use sqlx::PgPool;
 
 #[derive(Clone)]
+#[non_exhaustive]
 pub struct AppState {
     pub db: PgPool,
+}
+
+impl AppState {
+    #[inline]
+    pub const fn new(db: PgPool) -> Self {
+        AppState { db }
+    }
 }
