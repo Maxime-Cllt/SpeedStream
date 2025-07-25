710240a4919405d6b5c1bf50295e175364f8349f
Maxime Colliat <mcolliat21@gmail.com>
2025-07-25 15:51:41 +0200

    fix http server not reaching postgres

--- src/constant.rs
+++ src/constant.rs
@@ -1,1 +1,1 @@
-pub const DATABASE_URL: &str = "postgresql://speedstream:speedstream123@localhost:5432/speedstream_db";
+pub const DATABASE_URL: &str = "postgresql://speedstream:speedstream123@postgres:5432/speedstream_db";
